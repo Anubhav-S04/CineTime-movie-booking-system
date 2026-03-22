bced03c8ab89963b493282e7d39b3837bbe28778
Anubhav Saha <anubhavsaha142@gmail.com>
2026-03-22 16:54:36 +0530

    header and footer

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,19 @@
 import { Routes, Route } from "react-router-dom";
+import Header from "./components/shared/Header";
+import Footer from "./components/shared/Footer";
 function App() {
 
   return (
     <>
       <div className="flex flex-col min-h-screen">
         <main className="flex-grow">
+          <Header />
           <Routes>
             <Route path="/" element={<h1>Home page</h1>} />
             <Route path="/profile/:id" element={<h1>Profile page</h1>} />
             <Route path="/movies" element={<h1>Movies page</h1>} />
           </Routes>
+          <Footer />
         </main>
       </div>
     </>
---
+++ frontend/src/components/shared/Header.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div className='w-full text-sm bg-white'>Header</div>
+    )
+}
+export default Header;
