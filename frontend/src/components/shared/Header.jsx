634b0d9884d0fbf68781c9fecd4626fd21bb864a
Anubhav Saha <anubhavsaha142@gmail.com>
2026-03-24 01:15:44 +0530

    real time location api

--- frontend/src/components/shared/Header.jsx
+++ frontend/src/components/shared/Header.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
 import mainLogo from "../../assets/main-icon.png";
 import { FaSearch } from "react-icons/fa";
+import { useLocation } from "../../context/LocationContext";
+import map from "../../assets/pin.gif";
 
 const Header = () => {
+    const { location, loading, error } = useLocation();
+
     return (
         <div className='w-full text-sm bg-white'>
             {/* top navbar */}
@@ -11,16 +15,27 @@ const Header = () => {
                     {/* left part */}
                     <div className='flex items-center space-x-4'>
                         <img src={mainLogo} alt="logo" className='h-8 object-contain cursor-pointer' />
-                    
-                    <div className="relative">
-                        <input type="text" placeholder="Search for Movies, Events, Plays, Sports and more..." className=" border border-gray-300 rounded px-4 py-1.5 w-[400px] text-sm outline-none bg-gray-100 placeholder:text-gray-500 focus:outline-none focus:ring-2 focus:ring-blue-500" />
-                        <FaSearch className="absolute right-2 top-1/2 transform -translate-y-1/2 text-gray-500" />
-                    </div>
+
+                        <div className="relative">
+                            <input type="text" placeholder="Search for Movies, Events, Plays, Sports and more..." className=" border border-gray-300 rounded px-4 py-1.5 w-[400px] text-sm outline-none bg-gray-100 placeholder:text-gray-500 focus:outline-none focus:ring-2 focus:ring-blue-500" />
+                            <FaSearch className="absolute right-2 top-1/2 transform -translate-y-1/2 text-gray-500 cursor-pointer" />
+                        </div>
                     </div>
                     {/* right part */}
                     <div className="flex items-center space-x-6">
                         <div className="text-sm font-medium cursor-pointer ">
-                            West Bengal &nbsp;▼
+                            {/* {
+                                if(location){
+                                    {<p>{location}&nbsp;▼</p>}
+                                }else{{<img src={map} alt="loading..." className='w-10 h-10 ' />}}
+                            } */}
+                            {!location && (
+                                <img src={map} alt="loading..." className="w-8 h-8" />
+                            )}
+
+                            {location && (
+                                <p>{location}&nbsp;▼</p>
+                            )}
                         </div>
                         <button className="px-4 py-1.5 rounded bg-red-500 cursor-pointer text-sm font-medium text-white hover:bg-red-600">
                             Sign in
@@ -33,20 +48,20 @@ const Header = () => {
                 <div className='max-w-screen-xl mx-auto flex justify-between items-center py-2 text-graay-700'>
                     <div className='flex items-center space-x-6 font-medium'>
                         <span className='cursor-pointer hover:text-red-500'>Movies</span>
-                         <span className='cursor-pointer hover:text-red-500'>Streams</span>
+                        <span className='cursor-pointer hover:text-red-500'>Streams</span>
                         <span className='cursor-pointer hover:text-red-500'>Events</span>
                         <span className='cursor-pointer hover:text-red-500'>Plays</span>
                         <span className='cursor-pointer hover:text-red-500'>Sports</span>
-                          <span className='cursor-pointer hover:text-red-500'>Activities</span>
+                        <span className='cursor-pointer hover:text-red-500'>Activities</span>
                     </div>
                     <div className="flex items-center space-x-6 text-sm">
                         <span className='cursor-pointer hover:underline'>ListYourShow</span>
                         <span className='cursor-pointer hover:underline'>Corporates</span>
-                          <span className='cursor-pointer hover:underline'>Offers</span>
+                        <span className='cursor-pointer hover:underline'>Offers</span>
                         <span className='cursor-pointer hover:underline'>Gift Cards</span>
                     </div>
                 </div>
-            
+
             </div>
         </div>
     )
